59e4bfebb900f732930e529faac1616ad10772fc
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2022-12-16 13:12:24 +0100

    added models for groups and friends

--- src/models/users.py
+++ src/models/users.py
@@ -1,7 +1,10 @@
-from sqlalchemy import String, Integer, Enum, Column, ForeignKey
-from sqlalchemy.orm import relationship
-from src.db.database import Base
-from src.models.groups import users_groups_association_table 
+from sqlalchemy import String, Integer, Enum, Column, ForeignKey, Table
+from sqlalchemy.orm import relationship, backref
+#from src.db.database import Base
+from sqlalchemy.orm import declarative_base
+#from src.models.friends import friends_association_table 
+
+Base = declarative_base()
 
 class Role(Base):
     __tablename__ = "roles"
@@ -10,7 +13,30 @@ class Role(Base):
     role = Column(String(255), nullable = False, unique = True)
     users = relationship("User", backref="role")
     
+friends_association_table = Table(
+    "friends_association_table",
+    Base.metadata,
+    Column("user_a_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("user_b_id", Integer,  ForeignKey("users.id"), primary_key=True)    
+)
 
+users_groups_association_table = Table(
+    "users_groups_association_table",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id")),
+    Column("group_id", ForeignKey("groups.id"))    
+)
+
+class Group(Base):
+    __tablename__ = "groups"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
+    
+    users = relationship(
+        "User", secondary=users_groups_association_table, back_populates="groups"
+    )
+    
 class User(Base):
     __tablename__ = "users"
     
@@ -21,12 +47,21 @@ class User(Base):
     email = Column(String(255), nullable = False, unique=True)
     hashed_password = Column(String(255))
     
+    friends = relationship("User",
+                          secondary = friends_association_table ,
+                          primaryjoin = (id == friends_association_table.c.user_a_id),
+                          secondaryjoin = (id == friends_association_table.c.user_b_id)
+                          )
+
 
     role_id = Column(Integer, ForeignKey(Role.id), nullable = False)
     
     groups = relationship(
         "Group", secondary=users_groups_association_table, back_populates="users"
     )
+    
+    
+
     
 
 
