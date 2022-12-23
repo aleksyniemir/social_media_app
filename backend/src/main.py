595656a03855103d932d32e7009bbf77582d93d1
Aleksander Niemirka <aleksander.niemirka@gmail.com>
2022-12-23 14:58:13 +0100

    login view, first connection from front to api

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import users
 from src.db.database import Base, engine
 # uncomment at the end of the project
@@ -14,6 +15,21 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 app = FastAPI()
 
 app.include_router(users.router)
+
+origins = [
+    "null",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000/",
+    "http://127.0.0.1:8080/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # uncomment at the end of the project
 # logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
